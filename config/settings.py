0d36138e172fe95c84be15341f1af316f925d13f
Sergey <nekrasov-nekras-nekrasov@mail.ru>
2025-09-06 10:55:03 +0300

    .

--- config/settings.py
+++ config/settings.py
@@ -9,8 +9,8 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
-
+#SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY="kt00^m9k%tlqi53b_q5jwljcl)8z7bzifq-a551+v0sq)-%f"
 STRIPE_API_KEY = os.getenv('STRIPE_API_KEY')
 
 DEBUG = True if os.getenv("DEBUG") == "True" else False
